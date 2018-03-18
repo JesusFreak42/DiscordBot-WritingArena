4d99e9122701b440c3f55aa9e4a831301989c697
Timothy <stuckinachristiannovel@gmail.com>
2018-03-18 14:21:10 -0500

    initial commit of project

---
+++ DiscordBot-WritingArena/Modules/Ping.cs
@@ -0,0 +1,41 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_WritingArena.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        Random die = new Random();
+
+        [Command("ping")]
+        public async Task pingAsync()
+        {
+            await ReplyAsync("Hello world!");
+        }
+
+        [Command("enter")]
+        public async Task enterAsync()
+        {
+            int whichVillain = die.Next(1, 3); //shall not exceed the number of items in villainList below
+            int wordGoal = die.Next(50, 500);
+            string[] villainList = new string[] { "fierce battle goat", "giant armored slug", "poison-spitting llama" };
+            
+            await ReplyAsync("You step into the arena and confront a " + villainList[whichVillain] + ". Spring 10 minutes and write " + wordGoal + " words to defeat it!");
+
+        }
+
+    }
+}
+
+/*List of things to defeat in the arena:
+ * battle goat
+ * giant armored slug
+ * poison-spitting llama
+ * 
+ */
+
+
---
+++ DiscordBot-WritingArena/MyBot.cs
@@ -0,0 +1,80 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using WebSocket4Net;
+
+namespace DiscordBot_WritingArena
+{   
+    class MyBot
+    {
+
+        private static void Main(string[] args) => new MyBot().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient disClient;
+        private CommandService disCommands;
+        private IServiceProvider disServices;
+
+        public async Task RunBotAsync()
+        {
+            disClient = new DiscordSocketClient();
+            disCommands = new CommandService();
+            
+            disServices = new ServiceCollection()
+                .AddSingleton(disClient)
+                .AddSingleton(disCommands)
+                .BuildServiceProvider();
+
+            string botToken = "NDI0NjAwMDA0MjI5ODU3Mjgy.DY8B7A.2rjMuKsd1ZejRr74JMRxqX8MbdA";
+
+            //event subscriptions
+            disClient.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await disClient.LoginAsync(TokenType.Bot, botToken);
+            await disClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage x)
+        {
+            Console.WriteLine(x);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            disClient.MessageReceived += HandleCommandAsync;
+
+            await disCommands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage x)
+        {
+            var message = x as SocketUserMessage;
+            int argPos = 0;
+
+            if (message is null || message.Author.IsBot) return;
+
+            if (message.HasStringPrefix("+arena ", ref argPos) || message.HasMentionPrefix(disClient.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(disClient, message);
+                var result = await disCommands.ExecuteAsync(context, argPos, disServices);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+
+        }
+    }
+}
+
+
